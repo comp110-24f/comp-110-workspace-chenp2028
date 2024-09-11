e92ef4125587897a1d4221a96554d2a60c7e7460
phoebe <kexing42@gmail.com>
2024-09-11 15:10:14 -0400

    updated p2

--- Practice/p2.py
+++ Practice/p2.py
@@ -2,13 +2,11 @@ def get_weather_report() -> str:
     weather: str = input("What is the weather?")
     if weather == "rainy" or weather == "cold":
         print("Bring a jacket")
-        return weather
     elif weather == "hot":
         print("Keep cool out there!")
-        return weather
     else:
         print("I don't recongnize this weather")
-        return weather
+    return weather
 
 
 get_weather_report()
